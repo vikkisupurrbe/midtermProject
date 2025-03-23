5274436b5b64d834f47b552e83f86ac84e42f6e7
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-03-22 19:55:24 -0700

    Add url_key to results table for results sharing

--- db/schema/05_results.sql
+++ db/schema/05_results.sql
@@ -6,6 +6,7 @@ CREATE TABLE results (
   id SERIAL PRIMARY KEY NOT NULL,
   quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  total_questions INTEGER NOT NULL,
-  correct_answers INTEGER NOT NULL
+  total_questions SMALLINT NOT NULL,
+  correct_answers SMALLINT NOT NULL,
+  url_key VARCHAR(255)
 );
--- db/seeds/05_results.sql
+++ db/seeds/05_results.sql
@@ -1,22 +1,21 @@
-INSERT INTO results (quiz_id, user_id, total_questions, correct_answers)
-VALUES
-(1, 1, 4, 3),  -- User 1 took Quiz 1, 3 correct out of 4
-(2, 2, 4, 2),  -- User 2 took Quiz 2, 2 correct out of 4
-(3, 3, 4, 4),  -- User 3 took Quiz 3, 4 correct out of 4
-(4, 4, 4, 1),  -- User 4 took Quiz 4, 1 correct out of 4
-(5, 5, 4, 3),  -- User 5 took Quiz 5, 3 correct out of 4
-(6, 6, 4, 2),  -- User 6 took Quiz 6, 2 correct out of 4
-(7, 7, 4, 4),  -- User 7 took Quiz 7, 4 correct out of 4
-(8, 8, 4, 0),  -- User 8 took Quiz 8, 0 correct out of 4
-(9, 9, 4, 3),  -- User 9 took Quiz 9, 3 correct out of 4
-(10, 10, 4, 2), -- User 10 took Quiz 10, 2 correct out of 4
-(1, 2, 4, 4), -- User 2 took quiz 1, 4 correct.
-(2, 3, 4, 3), -- user 3 took quiz 2, 3 correct.
-(3, 4, 4, 2), -- User 4 took quiz 3, 2 correct.
-(4, 5, 4, 4), -- user 5 took quiz 4, 4 correct.
-(5, 6, 4, 0), -- user 6 took quiz 5, 0 correct.
-(6, 7, 4, 1), -- user 7 took quiz 6, 1 correct.
-(7, 8, 4, 2), -- user 8 took quiz 7, 2 correct.
-(8, 9, 4, 3), -- user 9 took quiz 8, 3 correct.
-(9, 10, 4, 4), -- user 10 took quiz 9, 4 correct.
-(10, 1, 4, 1); -- user 1 took quiz 10, 1 correct.
+INSERT INTO results (quiz_id, user_id, total_questions, correct_answers, url_key) VALUES
+(1, 1, 4, 3, 'aB7xY9z2w1'),
+(2, 2, 4, 2, 'cDeFgH3i4j'),
+(3, 3, 4, 4, 'iJkLmN5o6p'),
+(4, 4, 4, 1, 'oPqRsT7u8v'),
+(5, 5, 4, 3, 'uVwXyZ9a0b'),
+(6, 6, 4, 2, '1a2b3c4d5e'),
+(7, 7, 4, 4, '4d5e6f7g8h'),
+(8, 8, 4, 0, '7g8h9i0jkl'),
+(9, 9, 4, 3, 'jKlMn0pQrS'),
+(10, 10, 4, 2, 'pQrStUvWxY'),
+(1, 2, 4, 4, 'vWxYz1a2b3'),
+(2, 3, 4, 3, '2b3c4d5e6f'),
+(3, 4, 4, 2, '5e6f7g8h9i'),
+(4, 5, 4, 4, '8h9i0jKlMn'),
+(5, 6, 4, 0, 'jKmNoPqRsT'),
+(6, 7, 4, 1, 'qRsTuVwXyZ'),
+(7, 8, 4, 2, 'wXyZ1a2b3c'),
+(8, 9, 4, 3, '2b3c4d5e6f'),
+(9, 10, 4, 4, '5e6f7g8h9i'),
+(10, 1, 4, 1, '8h9i0jKlMn');
