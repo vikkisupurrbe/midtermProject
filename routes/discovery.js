a5418b6086a6090537d89c57650e2b2a39739173
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-03-23 14:32:59 -0700

    GET /:quiz_id render quizz.ejs with tempolary templateVars to test it works.

--- routes/discovery.js
+++ routes/discovery.js
@@ -3,21 +3,26 @@
 // View public quizzes on the home page
 // Share a quiz
 
-/* Remember to create your feature branch before starting 
+/* Remember to create your feature branch before starting
   git command:
-  ~ git checkout -b feature/quiz-discovery-jumpei 
+  ~ git checkout -b feature/quiz-discovery-jumpei
     (create and switch to a new branch)
-  ~ git branch 
+  ~ git branch
     (gives feature/quiz-discovery-jumpei)
   ~ git push origin feature/quiz-discovery-jumpei
 */
 
-const express = require('express');
-const router  = express.Router();
+const express = require("express");
+const router = express.Router();
 
 // Get a list of public quizzes
 router.get("/", (req, res) => {
-  res.status(200).json({ message: "List of public quizzes" });
+  // res.status(200).json({ message: "List of public quizzes" });0
+  // const templateVars = {
+  //   title: "General knowledgh 101",
+  //   description: "A basic quiz on various topics.",
+  // };
+  res.redirect("/" /*, templateVars*/);
 });
 
 // Get public quizzes for the homepage
@@ -27,7 +32,25 @@ router.get("/public", (req, res) => {
 
 // Get a specific quiz by ID
 router.get("/:quiz_id", (req, res) => {
-  res.status(200).json({ message: `Quiz ${req.params.quiz_id} details` });
+  // res.status(200).json({ message: `Quiz ${req.params.quiz_id} details` });
+  // const queryString = `
+  //   SELECT *
+  //   FROM quizzes
+  //   WHERE quizzes.id = $1;
+  // `;
+  // const queryParams = [req.params.quiz_id];
+
+  // return db.query(queryString, queryParams).then((res) => {
+  //   const templateVars = res.rows[0];
+  //   return res.render("quizz", templateVars);
+  // });
+
+  const templateVars = {
+    title: "General knowledgh 101",
+    description: "A basic quiz on various topics.",
+    url: "id",
+  };
+  res.render("quizz", templateVars);
 });
 
 module.exports = router;
