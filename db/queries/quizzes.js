507e6c16139de9060c19ae862946a8f517706ac4
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-03-23 13:00:53 -0700

    Implement queries for getQuizById, createResult and getResultsByUrl functions

---
+++ db/queries/quizzes.js
@@ -0,0 +1,57 @@
+const db = require('../connection');
+
+const getQuizById = function(id) {
+  const queryString =
+  `SELECT *
+   FROM quizzes
+   WHERE id = $1;
+  `;
+  const queryArgs = [id];
+
+  return db
+    .query(queryString, queryArgs)
+    .then((result) => result.rows.length > 0 ?
+      result.rows[0] : null)
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+const createResult = function(quiz_id, user_id, total_questions, correct_answers, url_key) {
+  const queryString =
+  `INSERT INTO results (quiz_id, user_id, total_questions, correct_answers, url_key)
+   VALUES ($1, $2, $3, $4, $5)
+   RETURNING *;
+  `;
+  const queryArgs = [quiz_id, user_id, total_questions, correct_answers, url_key];
+
+  return db
+    .query(queryString, queryArgs)
+    .then((result) => result.rows[0])
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+const getResultsByUrl = function(url_key) {
+  const queryString =
+  `SELECT users.name, total_questions, correct_answers, quizzes.title
+   FROM results
+   JOIN quizzes
+   ON quizzes.id = quiz_id
+   JOIN users
+   ON users.id = user_id
+   WHERE url_key = $1;
+  `;
+  const queryArgs = [url_key];
+
+  return db
+    .query(queryString, queryArgs)
+    .then((result) => result.rows.length > 0 ?
+      result.rows[0] : null)
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+module.exports = { getQuizById, getResultsByUrl, createResult };
